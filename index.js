5a347e6e931f36a2139d07a38878dd990c213578
aswathpandiyan <aswathpandiyan@gmail.com>
2021-09-17 21:16:11 +0530

    rethink db added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var SSE = require("sse-emitter");
 const bodyParser = require("body-parser");
+var r = require("rethinkdbdash")();
 
 const EventEmitter = require("events");
 class MyEmitter extends EventEmitter {}
@@ -15,13 +16,88 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// create helper middleware so we can reuse server-sent events
+const useServerSentEventsMiddleware = (req, res, next) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+
+  // only if you want anyone to access this endpoint
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.flushHeaders();
+
+  const sendEventStreamData = (event, data) => {
+    const sseFormattedResponse = `'event: ${event}\ndata: ${JSON.stringify(
+      data
+    )}\n\n`;
+    res.write(sseFormattedResponse);
+  };
+
+  // we are attaching sendEventStreamData to res, so we can use it later
+  Object.assign(res, {
+    sendEventStreamData,
+  });
+
+  next();
+};
+
+const streamRandomNumbers = (req, res) => {
+  // We are sending anyone who connects to /stream-random-numbers
+  // a random number that's encapsulated in an object
+  let interval = setInterval(function generateAndSendRandomNumber() {
+    const data = {
+      value: Math.random(),
+    };
+
+    res.sendEventStreamData(data);
+  }, 1000);
+
+  // close
+  res.on("close", () => {
+    clearInterval(interval);
+    res.end();
+  });
+};
+const sendEvent = function (error, result) {
+  if (error) throw error;
+};
+const streamErrors = (req, res) => {
+  r.table("errors")
+    .changes()
+    .run()
+    .then(function (result) {
+      result.each(function (err, row) {
+        if (err) throw err;
+
+        //console.log(data);
+        res.sendEventStreamData("update", row);
+        res.on("close", () => {
+          res.end();
+        });
+      });
+    });
+};
+app.get(
+  "/stream-random-numbers",
+  useServerSentEventsMiddleware,
+  streamRandomNumbers
+);
+
+app.get("/errors", useServerSentEventsMiddleware, streamErrors);
+
 app.get("/channel/:id", sse.bind());
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/pubilc/index.html"));
 });
+
 app.post("/trigger-sse", (req, res) => {
-  webhookEmitter.emit("onhook", req.body);
-  res.json({ success: true });
+  //webhookEmitter.emit("onhook", req.body);
+  r.table("errors")
+    .insert(req.body)
+    .run()
+    .then((result) => {
+      res.json({ success: true, message: result });
+    });
 });
 
 app.listen(5000);
